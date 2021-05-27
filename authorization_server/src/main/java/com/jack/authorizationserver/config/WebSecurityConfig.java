b6b013e662b8ad54ec88d83e06682d3711b40a84
zhuangzi200323 <zhuangzi200323@163.com>
2021-05-27 13:05:11 +0800

    enable csrf

--- authorization_server/src/main/java/com/jack/authorizationserver/config/WebSecurityConfig.java
+++ authorization_server/src/main/java/com/jack/authorizationserver/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http//.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/r/r1").hasAnyAuthority("p1")
                 .antMatchers("/login*").permitAll()
--- resource_server/src/main/java/com/jack/resourceserver/config/SecurityConfig.java
+++ resource_server/src/main/java/com/jack/resourceserver/config/SecurityConfig.java
@@ -11,7 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http//.csrf().disable()
                 .authorizeRequests()
                 .anyRequest().permitAll();
     }
