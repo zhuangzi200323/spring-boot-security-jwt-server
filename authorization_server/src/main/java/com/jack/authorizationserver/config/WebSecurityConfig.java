18f5f4916652077845950f13e330ffed3168ba77
zhuangzi200323 <zhuangzi200323@163.com>
2021-05-27 14:07:21 +0800

    crsf忽略druid

--- authorization_server/src/main/java/com/jack/authorizationserver/config/WebSecurityConfig.java
+++ authorization_server/src/main/java/com/jack/authorizationserver/config/WebSecurityConfig.java
@@ -25,9 +25,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http//.csrf().disable()
+        http.csrf().ignoringAntMatchers("/druid/*")
+                .and()
                 .authorizeRequests()
-                .antMatchers("/r/r1").hasAnyAuthority("p1")
                 .antMatchers("/login*").permitAll()
                 .anyRequest().authenticated()
                 .and()
